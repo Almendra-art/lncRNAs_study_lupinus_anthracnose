b925d201a09df8f074a2cb52427bfa41372611d1
Almendra Paz Hidalgo Cabrera <almendra.paz.hc@gmail.com>
2026-03-17 11:21:11 -0300

    Update run04_gffcompare.sh

--- 01_preprocessing/run04_gffcompare.sh
+++ 01_preprocessing/run04_gffcompare.sh
@@ -1,3 +1,3 @@
 #GFFcompare
-#run in your ubuntu terminal: bash run04_gffcompare.sh
+#NOTE1: run in your ubuntu terminal: bash run04_gffcompare.sh
 gffcompare -r your_path/reference_genome.gtf  -G your_path/stringtie_merged.gtf 
