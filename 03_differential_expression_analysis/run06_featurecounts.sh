8564932879c8c3fa3d01e5a3e905e286782c2ff0
Almendra Paz Hidalgo Cabrera <almendra.paz.hc@gmail.com>
2026-03-17 11:25:42 -0300

    Update run06_featurecounts.sh

--- 03_differential_expression_analysis/run06_featurecounts.sh
+++ 03_differential_expression_analysis/run06_featurecounts.sh
@@ -1,6 +1,4 @@
-#Create the count matrix to downstream analysis such as differential expression and co-expression, from .bam files created in run02_hisat_mapping.py 
-#Previously, a new_annotation.gtf file was constructed including annotated genes in the reference genome with "Llu" prefix, and the identifier of unannotated genes predicted as lncRNAs was changed with the "Llu_lncRNA" prefix. No identified genes in the genome and not predicted as lncRNAs, were identified with prefix "MSTRG".
-
+#Create the count matrix to downstream analysis such as differential expression and co-expression analysis from .bam files created in run02_hisat_mapping.py 
 #Run featurecounts in your ubuntu terminal and in the directory of .bam files : bash run06_featurecounts.sh
-
+#!/bin/bash
 featureCounts -T 8 -a your_path/new_annotation.gtf -o count_matrix.tsv *.bam
